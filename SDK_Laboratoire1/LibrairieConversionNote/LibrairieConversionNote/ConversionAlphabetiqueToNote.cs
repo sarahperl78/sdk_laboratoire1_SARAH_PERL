3a49c0ad239c3976412de8ade5482b42aaac777e
Sarah Perl <sarahperl78@gmail.com>
2023-06-16 21:29:16 -0400

    transformation alphabet en note

---
+++ SDK_Laboratoire1/LibrairieConversionNote/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("svp Veuillez inserer une note alphabétique parmis celles ci (A, B, C, D, E, F, G) :");
+            string noteAlphabetique = Console.ReadLine();
+
+            string noteMusicale = Transformer(noteAlphabetique);
+            Console.WriteLine(noteMusicale);
+            Console.WriteLine();
+        }
+    }
+
+    public static string Transformer(string noteAlphabetique)
+    {
+        switch (noteAlphabetique.ToUpper())
+        {
+            case "A":
+                return "DO";
+            case "B":
+                return "RÉ";
+            case "C":
+                return "MI";
+            case "D":
+                return "FA";
+            case "E":
+                return "SOL";
+            case "F":
+                return "LA";
+            case "G":
+                return "SI";
+            default:
+                return "cette note n'existe pas.";
+        }
+    }
+}
